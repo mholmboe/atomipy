b11f302c7d75cf5c5d28c15a18993a14cae2215d
Michael Holmboe <michael.holmboe@umu.se>
2026-04-22 14:37:59 +0200

    Fix diffraction imports and topology export in canonical library; sync with web module

---
+++ atom_MATLAB_functions/build_functions/place_atom.m
@@ -0,0 +1,34 @@
+%% place_atom.m
+% * This function places the atom struct according to the position vector 
+% called position, trying to use the COM of the molecule
+%
+%% Version
+% 3.00
+%
+%% Contact
+% Please report problems/bugs to michael.holmboe@umu.se
+%
+%% Function arguments
+% * position is a [1x3] vector
+%
+%% Examples
+% * atom = place_atom(atom,position)
+%
+function atom=place_atom(atom,position)
+
+if size(atom,2) < 500
+    atom = COM_atom(atom); % This generates the COM position through assignin. You should use unwrapped molecules
+
+    atom=rmfield(atom,'COM_x');
+    atom=rmfield(atom,'COM_y');
+    atom=rmfield(atom,'COM_z');
+    atom=rmfield(atom,'element');
+    atom=rmfield(atom,'Mw');
+
+else
+    COM=[mean([atom.x]) mean([atom.y]) mean([atom.z])]; % Since COM_atom is a bit slow for large molecules, we do this for big molecules
+end
+
+atom = translate_atom(atom,-COM+position,'all');
+
+end
---
+++ atom_MATLAB_functions/variables/XYZ_data_variable.m
@@ -0,0 +1,14 @@
+%% XYZ_data
+% * This variable is a nx3 array holding the xyz coordinates in Ångström
+% corresponding to the variable [[atom.x]' [atom.y]' [atom.z]']. The
+% motivation for this variable is simply to have all coordinates in the
+% same variable. The coordinates can be 'put back' to the <atom.html atom>
+% struct using the function <xyz2atom.html xyz2atom>
+%
+%% Version
+% 3.00
+%
+
+%% Example
+% # XYZ_data = [1 2 3; 4 5 6 ; 7 8 9]; % X data is then 1 4 7... and likewise for Y and Z
+XYZ_data = [1 2 3; 4 5 6 ; 7 8 9]
